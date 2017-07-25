7b5bdff9ff3a890d11147af8682040d7ddfba5db
van <vr.you@hotmail.fr>
2017-07-25 12:34:47 +0200

    fdsds

---
+++ demospringinjection/src/com/objis/spring/demo/SocietedevLogiciel.java
@@ -0,0 +1,21 @@
+package com.objis.spring.demo;
+
+public class SocietedevLogiciel {
+	
+	private Developpeur developpeur;
+	private Developpeur ChefDeveloppeur;
+	
+	public Developpeur getDeveloppeur() {
+		return developpeur;
+	}
+	public void setDeveloppeur(Developpeur developpeur) {
+		this.developpeur = developpeur;
+	}
+	public Developpeur getChefDeveloppeur() {
+		return ChefDeveloppeur;
+	}
+	public void setChefDeveloppeur(Developpeur chefDeveloppeur) {
+		ChefDeveloppeur = chefDeveloppeur;
+	}
+
+}
